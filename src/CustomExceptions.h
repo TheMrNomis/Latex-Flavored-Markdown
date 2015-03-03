85017e9746f042afc823af7589627e3fd5886176
TheMrNomis <n0m1s@homnomnom.fr>
2015-03-03 09:58:30 +0100

    Custom Exceptions (first commit)
    * new class CustomException
    	Base class for customs exceptions, basic what()
    * new class UnrecognizedArgument
    	Exception throwed when processing argv[] : if the option is not recognized
    * new class HelpMessage
    	Not "really" an exception, but is used to print the help text and stop the program when using `--help`

---
+++ src/CustomExceptions.h
@@ -0,0 +1,51 @@
+/*
+ * CustomExceptions.h
+ * This file is part of Latex-FlavoredMarkdown
+ *
+ * Copyright (C) 2015 - n0m1s
+ *
+ * Latex-FlavoredMarkdown is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Latex-FlavoredMarkdown is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Latex-FlavoredMarkdown. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __CUSTOM_EXCEPTIONS_H__
+#define __CUSTOM_EXCEPTIONS_H__
+
+#include <iostream>
+#include <exception>
+
+class CustomException : public std::exception
+{
+    public:
+        CustomException (std::string message);
+        virtual const char* what() const throw();
+    protected:
+        std::string m_message;
+};
+
+//----UnrecognizedArgument----
+class UnrecognizedArgument : public CustomException
+{
+    public:
+        UnrecognizedArgument (std::string arg);
+        virtual const char* what() const throw();
+};
+
+//----HelpMessage----
+class HelpMessage : public CustomException
+{
+    public:
+        HelpMessage (std::string helpText);
+};
+
+#endif /* __CUSTOM_EXCEPTIONS_H__ */
