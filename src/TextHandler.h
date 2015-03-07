9a174806de17d8b5f67f7c5c890c0c9102f4716e
TheMrNomis <n0m1s@homnomnom.fr>
2015-03-07 18:37:43 +0100

    New class : TextHandler
    Handles the text part of the md/tex file (basically, everything that is not maths)

---
+++ src/TextHandler.h
@@ -0,0 +1,37 @@
+/*
+ * TextHandler.h
+ * This file is part of Latex-Flavored-Markdown
+ *
+ * Copyright (C) 2015 - n0m1s
+ *
+ * Latex-Flavored-Markdown is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Latex-Flavored-Markdown is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Latex-Flavored-Markdown. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __TEXTHANDLER_H__
+#define __TEXTHANDLER_H__
+
+#include <iostream>
+#include "WritableElement.h"
+
+class TextHandler : public WritableElement
+{
+    public:
+        TextHandler();
+        virtual ~TextHandler();
+
+    private:
+        /* data */
+};
+
+#endif /* __TEXTHANDLER_H__ */
