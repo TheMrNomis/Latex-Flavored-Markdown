c64f8d5807536d6d13c3d9cffc871bed70ca93c1
TheMrNomis <n0m1s@homnomnom.fr>
2015-03-07 18:41:58 +0100

    new class: MathHandler
    handles tex maths mode ($$)

---
+++ src/MathHandler.cpp
@@ -0,0 +1,21 @@
+/*
+ * MathHandler.cpp
+ * This file is part of Latex-Flavored-Markdown
+ *
+ * Copyright (C) 2015 - n0m1s
+ *
+ * Latex-Flavored-Markdown is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Latex-Flavored-Markdown is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Latex-Flavored-Markdown. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "MathHandler.h"
---
+++ src/MathHandler.h
@@ -0,0 +1,37 @@
+/*
+ * MathHandler.h
+ * This file is part of Latex-Flavored-Markdown
+ *
+ * Copyright (C) 2015 - n0m1s
+ *
+ * Latex-Flavored-Markdown is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Latex-Flavored-Markdown is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Latex-Flavored-Markdown. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __MATHSHANDLER_H__
+#define __MATHSHANDLER_H__
+
+#include <iostream>
+#include "WritableElement.h"
+
+class MathHandler : public WritableElement
+{
+    public:
+        MathHandler();
+        virtual ~MathHandler();
+
+        virtual std::string toString() const;
+    private:
+};
+
+#endif /* __MATHSHANDLER_H__ */
