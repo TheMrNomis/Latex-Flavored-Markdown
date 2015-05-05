7e7f9a1e991254a229ad48352d3cc058b2b336cc
TheMrNomis <n0m1s@homnomnom.fr>
2015-05-05 19:16:20 +0200

    added the "Block" class

---
+++ include/Block.h
@@ -0,0 +1,51 @@
+/*
+ * Block.h
+ * This file is part of Latex-Flavored-Markdown
+ *
+ * Copyright (C) 2015 - n0m1s
+ *
+ * Latex-Flavored-Markdown is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Latex-Flavored-Markdown is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Latex-Flavored-Markdown. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __BLOCK_H__
+#define __BLOCK_H__
+
+#include <iostream>
+#include <list>
+#include "Configuration.h"
+
+class Block
+{
+  public:
+    Block (Configuration * conf);
+    virtual ~Block ();
+    
+    virtual void beginBlock() = 0;
+    virtual void addLine(std::string line, bool terminateLine = true) = 0;
+    virtual void stopBlock() = 0;
+    
+    virtual std::string transformSingleLine(std::string line) = 0;
+
+  private:
+    std::list<std::string> m_lines;
+    Configuration * m_conf;
+};
+
+Block::Block(Configuration * conf):
+  m_lines(),
+  m_conf(conf)
+{}
+
+#endif /* __BLOCK_H__ */
+
